aa9afc21d6042e59b8c33d21763e9527b6015e3a
Pablo Kornblum <pablo-kornblum@outlook.com>
2022-10-31 13:55:42 +0100

    removed enum, updated version;

--- server/index.ts
+++ server/index.ts
@@ -7,19 +7,13 @@ import './src/serverEvents';
 
 export const OSS = {
     name: 'OSS',
-    version: 'v1.0',
+    version: 'v1.1',
     interactionRange: 2,
     randomizeBuyers: false, // Will randomize output of vending machines as well.
     randomizeSellers: false, // Randomize drug dealer prices for examples (based on list.)
 };
 
-export enum OSS_TRANSLATIONS {
-    openShop = 'Open Shop',
-    openSellingShop = 'Open Shop',
-    notEnoughCash = 'Not enough cash!',
-}
-
 PluginSystem.registerPlugin(OSS.name, async () => {
-    alt.log(`~lg~${OSS.name} ${OSS.version} successfully loaded.`);
     ShopInitializer.startupShop();
+    alt.log(`~lg~${OSS.name} ${OSS.version} successfully loaded.`);
 });
