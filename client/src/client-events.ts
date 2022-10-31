f8d81eef91fe78b0fbfdb185250c53f2e826da57
Pablo Kornblum <pablo-kornblum@outlook.com>
2022-10-31 13:46:22 +0100

    renamed pagename and removed unused variable

--- client/src/client-events.ts
+++ client/src/client-events.ts
@@ -1,8 +1,8 @@
 import * as alt from 'alt-client';
 
-const PAGE_NAME = 'ShopUI';
+const PAGE_NAME = 'OSS_ShopUI';
 
-alt.onServer(`${PAGE_NAME}:Client:OpenShop`, (shopItems: {}[], type: string, shopImg: string) => {
-    alt.emit(`${PAGE_NAME}:Vue:Open`, shopItems, type, shopImg);
+alt.onServer(`${PAGE_NAME}:Client:OpenShop`, (shopItems: {}[], type: string) => {
+    alt.emit(`${PAGE_NAME}:Vue:Open`, shopItems, type);
     return;
 });
